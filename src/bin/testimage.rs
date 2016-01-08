7b50be196230761e75c2793e5de5c9c1fc3bf512
Maximilian Krüger <kruemaxi@googlemail.com>
2016-01-08 12:00:43 +0100

    compute horizontal evolving factor

--- src/bin/testimage.rs
+++ src/bin/testimage.rs
@@ -23,7 +23,7 @@ fn vertical_line<I: Iterator<Item = usize>>(rows: I, col: usize) -> Mat {
 }
 
 fn main() {
-    let mut factors: Vec<DMat<f64>> = Vec::new();
+    let mut factors: Vec<Mat> = Vec::new();
 
     factors.push(horizontal_line(9, 0..5));
     for row in (0..9).rev() {
@@ -35,8 +35,18 @@ fn main() {
         factors.push(vertical_line(0..10, col));
     }
 
+    println!("factors:\n");
     for factor in factors {
         println!("{:?}", factor);
     }
-}
 
+    let mut horizontal_evolving: Vec<Mat> = Vec::new();
+    for start_col in 0..6 {
+        horizontal_evolving.push(horizontal_line(9, start_col..(start_col + 5)));
+    }
+
+    println!("horizontal_evolving:\n");
+    for factor in horizontal_evolving {
+        println!("{:?}", factor);
+    }
+}
