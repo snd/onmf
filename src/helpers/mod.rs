8c2980787999a3312f49d83e0dd3304dfae65319
Maximilian Krüger <kruemaxi@googlemail.com>
2016-01-12 22:10:47 +0100

    add helpers module with random01 function and test

---
+++ src/helpers/mod.rs
@@ -0,0 +1,29 @@
+/*!
+helper functions used by this crate
+whose utility surpasses the boundaries of this crate.
+*/
+
+extern crate rand;
+use self::rand::{Rng, Closed01, Rand};
+
+mod repeatedly;
+pub use self::repeatedly::{Repeatedly, repeatedly};
+
+/// returns a random number between `0.` (inclusive) and `1.` (inclusive)
+pub fn random01<T, R>(rng: &mut R) -> T
+    where Closed01<T>: Rand,
+          R: Rng
+{
+    let Closed01(random) = rng.gen::<Closed01<T>>();
+    random
+}
+
+#[test]
+fn test_random01() {
+    use self::rand::{StdRng, SeedableRng};
+
+    let seed: &[_] = &[1, 2, 3, 4];
+    let mut rng: StdRng = SeedableRng::from_seed(seed);
+    let r: f64 = random01(&mut rng);
+    assert_eq!(r, 0.5162139860908154);
+}
