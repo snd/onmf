613be7717d3abfceceb965cb171db42f612dfa57
Maximilian Krüger <kruemaxi@googlemail.com>
2016-01-12 02:17:30 +0100

    first step into testimage generation

--- src/bin/testimage.rs
+++ src/bin/testimage.rs
@@ -1,158 +1,59 @@
-use std::ops::{Index, IndexMut, Mul, Add, Div};
-
-#[macro_use]
-extern crate quick_error;
+extern crate onmf;
+use onmf::testimage::Testimage;
 
 extern crate nalgebra;
-use nalgebra::{DMat, DVec};
-
-extern crate image;
-use image::{ImageBuffer, Luma, DynamicImage};
+use nalgebra::{DMat};
 
 extern crate rand;
 use rand::{StdRng, Rng, SeedableRng, Closed01};
 
-type FloatType = f64;
-type Mat = DMat<FloatType>;
-
-fn horizontal_line<I: Iterator<Item = usize>>(row: usize, cols: I) -> Mat {
-    let mut factor: Mat = DMat::new_zeros(10, 10);
-    for col in cols {
-        *factor.index_mut((row, col)) = 1.;
-    }
-    factor
-}
-
-fn vertical_line<I: Iterator<Item = usize>>(rows: I, col: usize) -> Mat {
-    let mut factor: Mat = DMat::new_zeros(10, 10);
-    for row in rows {
-        *factor.index_mut((row, col)) = 1.;
-    }
-    factor
-}
-
-fn mat_to_image(mat: &Mat) -> DynamicImage {
-    let mut image_buffer
-        = ImageBuffer::new(mat.ncols() as u32, mat.nrows() as u32);
-    for (x, y, pixel) in image_buffer.enumerate_pixels_mut() {
-        let value = mat.index((y as usize, x as usize));
-        let byte = (*value * 255.).round() as u8;
-        *pixel = Luma([byte]);
-    }
-    DynamicImage::ImageLuma8(image_buffer)
-}
-
-quick_error! {
-    #[derive(Debug)]
-    pub enum ImageSaveError {
-        Io(err: std::io::Error) {}
-        Image(err: image::ImageError) {}
-    }
-}
-
-fn save_as_png(mat: &Mat, filename: &str) -> Result<(), ImageSaveError> {
-    let image = mat_to_image(&mat);
-    let path = std::path::Path::new(filename);
-    let mut file = try!(std::fs::File::create(&path)
-                        .map_err(ImageSaveError::Io));
-    image.save(&mut file, image::PNG).map_err(ImageSaveError::Image)
-}
-
-fn linear_combination(factors: &[Mat], coefficients: &[FloatType]) -> Mat {
-    assert!(0 < factors.len());
-    assert_eq!(factors.len(), coefficients.len());
-    let nrows = factors[0].nrows();
-    let ncols = factors[0].ncols();
-    let mut result = DMat::new_zeros(nrows, ncols);
-    for (factor, coefficient) in factors.iter().zip(coefficients.iter()) {
-        assert_eq!(factor.nrows(), nrows);
-        assert_eq!(factor.ncols(), ncols);
-        result = result.add(factor.clone().mul(coefficient.clone()));
-    }
-    // result.div(factors.len() as FloatType)
-    result
-}
-
-fn random_coefficient<R: Rng>(rng: &mut R) -> FloatType {
-    let Closed01(random) = rng.gen::<Closed01<FloatType>>();
-    random
-}
-
-fn random_coefficients<R: Rng>(rng: &mut R, count: usize) -> DVec<FloatType> {
-    let mut coefficients: DVec<FloatType> = DVec::new_zeros(count);
-    for i in 0..count {
-        coefficients[i] = random_coefficient(rng);
-    }
-    coefficients
-}
-
 fn main() {
-    let mut factors: Vec<Mat> = Vec::new();
-
-    factors.push(horizontal_line(9, 0..5));
-    for row in (0..9).rev() {
-        factors.push(horizontal_line(row, 0..10));
+    let static_factors: Vec<DMat<f64>> = Testimage::static_factors().collect();
+    for (i, factor) in static_factors.iter().enumerate() {
+        factor.save_luma01_to_png(&format!("factor-{}.png", i)[..]).unwrap();
     }
 
-    factors.push(vertical_line(5..10, 0));
-    for col in 1..10 {
-        factors.push(vertical_line(0..10, col));
+    let horizontal_evolving_factors: Vec<DMat<f64>> = Testimage::horizontal_evolving_factors().collect();
+    for (i, factor) in horizontal_evolving_factors.iter().enumerate() {
+        factor.save_luma01_to_png(&format!("horizontal-{}.png", i)[..]).unwrap();
     }
 
-    println!("factors:\n");
-    for (i, factor) in factors.iter().enumerate() {
-        save_as_png(&factor, &format!("factor-{}.png", i)[..]).unwrap();
-        println!("{:?}", factor);
+    let vertical_evolving_factors: Vec<DMat<f64>> = Testimage::vertical_evolving_factors().collect();
+    for (i, factor) in vertical_evolving_factors.iter().enumerate() {
+        factor.save_luma01_to_png(&format!("vertical-{}.png", i)[..]).unwrap();
     }
 
-    let mut horizontal_evolving: Vec<Mat> = Vec::new();
-    for start_col in 0..6 {
-        horizontal_evolving.push(horizontal_line(9, start_col..(start_col + 5)));
+    let testimages: onmf::testimage::Test<DMat<f64>> = Testimage::testimages();
+    for (i, factor) in testimages.enumerate() {
+        factor.save_luma01_to_png(&format!("test-{}.png", i)[..]).unwrap();
     }
 
-    println!("short horizontal bar that moves from left to right:\n");
-    for (i, factor) in horizontal_evolving.iter().enumerate() {
-        save_as_png(&factor, &format!("horizontal-{}.png", i)[..]).unwrap();
-        println!("{:?}", factor);
-    }
-
-    let mut vertical_evolving: Vec<Mat> = Vec::new();
-    for start_row in (0..6).rev() {
-        vertical_evolving.push(vertical_line(start_row..(start_row + 5), 0));
-    }
-
-    println!("short vertical bar that moves from bottom to top:\n");
-    for (i, factor) in vertical_evolving.iter().enumerate() {
-        save_as_png(&factor, &format!("vertical-{}.png", i)[..]).unwrap();
-        println!("{:?}", factor);
-    }
-
-    let seed: &[_] = &[1, 2, 3, 4];
-    let mut rng: StdRng = SeedableRng::from_seed(seed);
-
+    // let seed: &[_] = &[1, 2, 3, 4];
+    // let mut rng: StdRng = SeedableRng::from_seed(seed);
+    //
     // for i in 0..10 {
     //     let combination = random_linear_combination(&factors[..], &mut rng);
     //     save_as_png(&combination, &format!("combination-{}.png", i)[..]).unwrap();
     // }
 
-    let iter = horizontal_evolving.iter()
-        .zip(vertical_evolving.iter()).enumerate();
-    for (step, (horizontal, vertical)) in iter {
-        for i in 0..10 {
-            // static factors
-            let random_coefficients = random_coefficients(&mut rng, factors.len()).div(4.);
-            let result = linear_combination(&factors[..], random_coefficients.as_slice());
-
-            // evolving factors
-            let horizontal_coefficient = random_coefficient(&mut rng) / 4.;
-            let result = result.add(
-                horizontal.clone().mul(horizontal_coefficient));
-
-            let vertical_coefficient = random_coefficient(&mut rng) / 4.;
-            let result = result.add(
-                vertical.clone().mul(vertical_coefficient));
-
-            save_as_png(&result, &format!("test-{}-{}.png", step, i)[..]).unwrap();
-        }
-    }
+//     let iter = horizontal_evolving.iter()
+//         .zip(vertical_evolving.iter()).enumerate();
+//     for (step, (horizontal, vertical)) in iter {
+//         for i in 0..10 {
+//             // static factors
+//             let random_coefficients = random_coefficients(&mut rng, factors.len()).div(4.);
+//             let result = linear_combination(&factors[..], random_coefficients.as_slice());
+//
+//             // evolving factors
+//             let horizontal_coefficient = random_coefficient(&mut rng) / 4.;
+//             let result = result.add(
+//                 horizontal.clone().mul(horizontal_coefficient));
+//
+//             let vertical_coefficient = random_coefficient(&mut rng) / 4.;
+//             let result = result.add(
+//                 vertical.clone().mul(vertical_coefficient));
+//
+//             save_as_png(&result, &format!("test-{}-{}.png", step, i)[..]).unwrap();
+//         }
+//     }
 }
--- src/testimage.rs
+++ src/testimage.rs
@@ -7,7 +7,7 @@ use std;
 use std::cmp::PartialOrd;
 
 use std::ops::Range;
-use std::iter::{Map, Rev, once, Once, Chain};
+use std::iter::{Map, Rev, once, Once, Chain, Zip};
 
 extern crate num;
 use num::traits::{Zero, One, FromPrimitive, ToPrimitive, Float};
@@ -31,6 +31,7 @@ quick_error! {
 pub type Horizontal<T> = Map<Range<usize>, fn(usize) -> T>;
 pub type Vertical<T> = Map<Rev<Range<usize>>, fn(usize) -> T>;
 pub type Static<T> = Chain<Chain<Chain<Once<T>, Vertical<T>>, Once<T>>, Horizontal<T>>;
+pub type Test<T> = Map<Zip<Horizontal<T>, Vertical<T>>, fn((T, T)) -> T>;
 
 // TODO move the type constraints back up here
 pub trait Testimage {
@@ -66,7 +67,7 @@ pub trait Testimage {
               Self: Sized + Mul<C, Output = Self>,
               Self::Item: Clone + Copy + Zero;
     fn normalize(&self) -> Self where Self::Item: Clone + BaseFloat;
-    // fn testimages() -> Box<Iterator<Item=Self>> where Self: Sized, Self::Item: Clone + Copy + Zero + One + BaseFloat;
+    fn testimages() -> Test<Self> where Self::Item: Clone + Copy + Zero + One + BaseFloat;
 }
 
 impl<T> Testimage for DMat<T> {
@@ -74,136 +75,135 @@ impl<T> Testimage for DMat<T> {
 
     fn new_horizontal_line<I: Iterator<Item = usize>>(row: usize, cols: I) -> Self
         where Self::Item: Clone + Copy + Zero + One
-        {
-            let mut factor: DMat<Self::Item> = DMat::new_zeros(10, 10);
-            for col in cols {
-                *factor.index_mut((row, col)) = Self::Item::one();
-            }
-            factor
+    {
+        let mut factor: DMat<Self::Item> = DMat::new_zeros(10, 10);
+        for col in cols {
+            *factor.index_mut((row, col)) = Self::Item::one();
         }
+        factor
+    }
 
     fn new_vertical_line<I: Iterator<Item = usize>>(rows: I, col: usize) -> Self
         where Self::Item: Clone + Copy + Zero + One
-        {
-            let mut factor: DMat<Self::Item> = DMat::new_zeros(10, 10);
-            for row in rows {
-                *factor.index_mut((row, col)) = Self::Item::one();
-            }
-            factor
+    {
+        let mut factor: DMat<Self::Item> = DMat::new_zeros(10, 10);
+        for row in rows {
+            *factor.index_mut((row, col)) = Self::Item::one();
         }
+        factor
+    }
 
     fn luma01_to_image(&self) -> DynamicImage
         where Self::Item: Zero + One + FromPrimitive + ToPrimitive + PartialOrd + Float
-        {
-            let mut image_buffer = ImageBuffer::new(self.ncols() as u32, self.nrows() as u32);
-            for (x, y, pixel) in image_buffer.enumerate_pixels_mut() {
-                let value = self.index((y as usize, x as usize));
-                assert!(Self::Item::zero() <= *value);
-                assert!(*value <= Self::Item::one());
-                let max = Self::Item::from_u8(std::u8::MAX).unwrap();
-                let byte = (*value * max).round().to_u8().unwrap();
-                *pixel = Luma([byte]);
-            }
-            DynamicImage::ImageLuma8(image_buffer)
+    {
+        let mut image_buffer = ImageBuffer::new(self.ncols() as u32, self.nrows() as u32);
+        for (x, y, pixel) in image_buffer.enumerate_pixels_mut() {
+            let value = self.index((y as usize, x as usize));
+            assert!(Self::Item::zero() <= *value);
+            assert!(*value <= Self::Item::one());
+            let max = Self::Item::from_u8(std::u8::MAX).unwrap();
+            let byte = (*value * max).round().to_u8().unwrap();
+            *pixel = Luma([byte]);
         }
+        DynamicImage::ImageLuma8(image_buffer)
+    }
 
     fn save_luma01_to_png(&self, filename: &str) -> Result<(), ImageSaveError>
         where Self::Item: Zero + One + FromPrimitive + ToPrimitive + PartialOrd + Float
-        {
-            let image = self.luma01_to_image();
-            let path = std::path::Path::new(filename);
-            let mut file = try!(std::fs::File::create(&path).map_err(ImageSaveError::Io));
-            image.save(&mut file, image::PNG).map_err(ImageSaveError::Image)
-        }
+    {
+        let image = self.luma01_to_image();
+        let path = std::path::Path::new(filename);
+        let mut file = try!(std::fs::File::create(&path).map_err(ImageSaveError::Io));
+        image.save(&mut file, image::PNG).map_err(ImageSaveError::Image)
+    }
 
     fn static_factors() -> Static<Self>
         where Self::Item: Clone + Copy + Zero + One
-        {
-            fn helper_horizontal<U: Clone + Copy + Zero + One>(row: usize) -> DMat<U> {
-                Testimage::new_horizontal_line(row, 0..10)
-            }
-            fn helper_vertical<U: Clone + Copy + Zero + One>(col: usize) -> DMat<U> {
-                Testimage::new_vertical_line(0..10, col)
-            }
-            once(Testimage::new_horizontal_line(9, 0..5))
-                .chain((0..9).rev().map(helper_horizontal as fn(usize) -> Self))
-                .chain(once(Testimage::new_vertical_line(5..10, 0)))
-                .chain((1..10).map(helper_vertical as fn(usize) -> Self))
+    {
+        fn helper_horizontal<U: Clone + Copy + Zero + One>(row: usize) -> DMat<U> {
+            Testimage::new_horizontal_line(row, 0..10)
         }
+        fn helper_vertical<U: Clone + Copy + Zero + One>(col: usize) -> DMat<U> {
+            Testimage::new_vertical_line(0..10, col)
+        }
+        once(Testimage::new_horizontal_line(9, 0..5))
+            .chain((0..9).rev().map(helper_horizontal as fn(usize) -> Self))
+            .chain(once(Testimage::new_vertical_line(5..10, 0)))
+            .chain((1..10).map(helper_vertical as fn(usize) -> Self))
+    }
 
     fn horizontal_evolving_factors() -> Horizontal<Self>
         where Self::Item: Clone + Copy + Zero + One
-        {
-            fn helper<U: Clone + Copy + Zero + One>(start_col: usize) -> DMat<U> {
-                Testimage::new_horizontal_line(9, start_col..(start_col + 5))
-            }
-            (0..6).map(helper)
+    {
+        fn helper<U: Clone + Copy + Zero + One>(start_col: usize) -> DMat<U> {
+            Testimage::new_horizontal_line(9, start_col..(start_col + 5))
         }
+        (0..6).map(helper)
+    }
 
     fn vertical_evolving_factors() -> Vertical<Self>
         where Self::Item: Clone + Copy + Zero + One
-        {
-            fn helper<U: Clone + Copy + Zero + One>(start_row: usize) -> DMat<U> {
-                Testimage::new_vertical_line(start_row..(start_row + 5), 0)
-            }
-            (0..6).rev().map(helper)
+    {
+        fn helper<U: Clone + Copy + Zero + One>(start_row: usize) -> DMat<U> {
+            Testimage::new_vertical_line(start_row..(start_row + 5), 0)
         }
+        (0..6).rev().map(helper)
+    }
 
     fn linear_combination<C>(factors: &[Self], coefficients: &[C]) -> Self
         where C: Clone,
               Self: Mul<C, Output = Self>,
               Self::Item: Clone + Copy + Zero
-              {
-                  assert!(0 < factors.len());
-                  assert_eq!(factors.len(), coefficients.len());
-                  let nrows = factors[0].nrows();
-                  let ncols = factors[0].ncols();
-                  let mut result = DMat::new_zeros(nrows, ncols);
-                  for (factor, coefficient) in factors.iter().zip(coefficients.iter()) {
-                      assert_eq!(factor.nrows(), nrows);
-                      assert_eq!(factor.ncols(), ncols);
-                      result = result.add(factor.clone().mul(coefficient.clone()));
-                  }
-                  result
-              }
+    {
+        assert!(0 < factors.len());
+        assert_eq!(factors.len(), coefficients.len());
+        let nrows = factors[0].nrows();
+        let ncols = factors[0].ncols();
+        let mut result = DMat::new_zeros(nrows, ncols);
+        for (factor, coefficient) in factors.iter().zip(coefficients.iter()) {
+            assert_eq!(factor.nrows(), nrows);
+            assert_eq!(factor.ncols(), ncols);
+            result = result.add(factor.clone().mul(coefficient.clone()));
+        }
+        result
+    }
 
     fn normalize(&self) -> Self
         where Self::Item: Clone + BaseFloat
-        {
-            let nrows = self.nrows();
-            let ncols = self.ncols();
-            let mut vec = DVec::from_slice(nrows * ncols, self.as_vec());
-            vec.normalize_mut();
-            DMat::from_col_vec(nrows, ncols, vec.as_slice())
-        }
+    {
+        let nrows = self.nrows();
+        let ncols = self.ncols();
+        let mut vec = DVec::from_slice(nrows * ncols, self.as_vec());
+        vec.normalize_mut();
+        DMat::from_col_vec(nrows, ncols, vec.as_slice())
+    }
 
-// fn testimages() -> Box<Iterator<Item=Self>>
-//     where Self: Sized, Self::Item: Clone + Copy + Zero + One + BaseFloat
-// {
-//     let () =
-//     // let static_factors: Vec<Self> = Testimage::static_factors();
-//     // horizontal does not survive this scope
-//     // Box is returned and contains horizontal
-//     let horizontal: Box<Iterator<Item=Self>> =
-//         Testimage::horizontal_evolving_factors();
-//     let vertical: Box<Iterator<Item=Self>> =
-//         Testimage::vertical_evolving_factors();
-//     // using `move` and `into_iter` we move everything
-//     // into the `Box` so it's still around when this function returns
-//     Box::new(horizontal.clone())
-//     // Box::new(
-//     //     horizontal
-//     //     .zip(vertical)
-//     //     .enumerate()
-//     //     .map(|(step, (horizontal, vertical))| {
-//     //         // let h: Self = horizontal;
-//     //         // // let cloned: Self = (*horizontal).clone::<Self>();
-//     //         // let foo = h.add(vertical);
-//     //         // let result: Self = foo.normalize::<Self>();
-//     //         // result
-//     //         horizontal.clone()
-//     //     }))
-// }
+    fn testimages() -> Test<Self>
+        where Self::Item: Clone + Copy + Zero + One + BaseFloat
+    {
+        fn helper<U, V, W>((a, b): (U, V)) -> W
+            where U: Add<V, Output = W>,
+                  W: Testimage,
+                  W::Item: Clone + BaseFloat
+        {
+            a.add(b).normalize()
+        }
+        Testimage::horizontal_evolving_factors()
+            .zip(Testimage::vertical_evolving_factors())
+            .map(helper)
+
+        //     horizontal
+        //     .zip(vertical)
+        //     .enumerate()
+        //     .map(|(step, (horizontal, vertical))| {
+        //         // let h: Self = horizontal;
+        //         // // let cloned: Self = (*horizontal).clone::<Self>();
+        //         // let foo = h.add(vertical);
+        //         // let result: Self = foo.normalize::<Self>();
+        //         // result
+        //         horizontal.clone()
+        //     }))
+    }
 }
 
 // fn random_coefficient<R: Rng>(rng: &mut R) -> FloatType {
