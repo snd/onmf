e32d264cfeb48116b5e07807a3924bd59c17659c
Maximilian Krüger <kruemaxi@googlemail.com>
2016-01-13 00:09:29 +0100

    add docstring

--- src/testimage_generator.rs
+++ src/testimage_generator.rs
@@ -1,3 +1,22 @@
+/*!
+generates images used to test NMF
+
+reproducably (seeded) generate test data.
+
+a series of pictures.
+each picture is generated from a linear combination of factors.
+the coefficients of the linear combination are chosen randomly.
+
+two evolving factors change over time.
+the two evolving factors change 6 times.
+we generate 1000 pictures for each change of the evolving factors
+during which the evolving factors stay constant.
+
+the series of pictures is fed into ONMF one at a time.
+a picture is fed as a 1D array (series of values, slice) into the ONMF
+and not as a 2D array.
+*/
+
 use std::ops::Range;
 use std::iter::{Map, Rev, once, Once, Chain, Zip};
 
