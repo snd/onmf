a83c62b37fdd7f1bdbe1a39517d0c196b420610c
Maximilian Krüger <kruemaxi@googlemail.com>
2016-01-12 22:32:22 +0100

    move random01 into its own file

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,12 +1,9 @@
 /*!
 helper functions used by this crate
-whose utility surpasses the boundaries of this crate.
+whose utility surpasses the confines of this crate.
 
 generic things that are also useful in other crates.
 */
-
-extern crate rand;
-use self::rand::{Rng, Closed01, Rand};
 
 mod repeatedly;
 pub use self::repeatedly::{Repeatedly, repeatedly};
@@ -17,21 +14,5 @@ pub use self::normalize::Normalize;
 mod partialmax;
 pub use self::partialmax::PartialMaxIteratorExt;
 
-/// returns a random number between `0.` (inclusive) and `1.` (inclusive)
-pub fn random01<T, R>(rng: &mut R) -> T
-    where Closed01<T>: Rand,
-          R: Rng
-{
-    let Closed01(random) = rng.gen::<Closed01<T>>();
-    random
-}
-
-#[test]
-fn test_random01() {
-    use self::rand::{StdRng, SeedableRng};
-
-    let seed: &[_] = &[1, 2, 3, 4];
-    let mut rng: StdRng = SeedableRng::from_seed(seed);
-    let r: f64 = random01(&mut rng);
-    assert_eq!(r, 0.5162139860908154);
-}
+mod random;
+pub use self::random::random01;
