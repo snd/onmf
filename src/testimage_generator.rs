51c74ea8db9040ddf213ac26c42cb13516fe36db
Maximilian Krüger <kruemaxi@googlemail.com>
2016-01-13 01:23:01 +0100

    add ImgGen

--- src/testimage_generator.rs
+++ src/testimage_generator.rs
@@ -17,7 +17,7 @@ a picture is fed as a 1D array (series of values, slice) into the ONMF
 and not as a 2D array.
 */
 
-use std::ops::Range;
+use std::ops::{Range, Div};
 use std::iter::{Map, Rev, once, Once, Chain, Zip};
 
 extern crate nalgebra;
@@ -25,6 +25,11 @@ use self::nalgebra::{DMat};
 
 extern crate num;
 use self::num::traits::{Zero, One};
+
+extern crate rand;
+use self::rand::{Rng, Closed01, Rand};
+
+use helpers::{random01, Normalize};
 
 pub type HorizontalReturnT<T> = Map<Range<usize>, fn(usize) -> T>;
 pub type VerticalReturnT<T> = Map<Rev<Range<usize>>, fn(usize) -> T>;
@@ -96,4 +101,81 @@ pub fn static_factors<T>() -> StaticReturnT<DMat<T>>
         .chain((0..9).rev().map(helper_horizontal as fn(usize) -> DMat<T>))
         .chain(once(vertical_line(5..10, 0)))
         .chain((1..10).map(helper_vertical as fn(usize) -> DMat<T>))
+}
+
+pub struct ImgGen<'a, R: 'a, T, IH, IV> {
+    pub per_step: usize,
+    pub step: usize,
+    pub i: usize,
+    pub horizontal_iter: IH,
+    pub vertical_iter: IV,
+    pub current_horizontal: T,
+    pub current_vertical: T,
+    pub static_factors: Vec<T>,
+    pub rng: &'a mut R,
+}
+
+impl<'a, R, T> ImgGen<'a, R, DMat<T>, HorizontalReturnT<DMat<T>>, VerticalReturnT<DMat<T>>>
+    where R: 'a,
+          T: Clone + Copy + Zero + One
+{
+    pub fn new(per_step: usize, rng: &'a mut R) -> Self {
+        assert!(0 < per_step);
+        let mut horizontal_iter = horizontal_evolving_factors();
+        let mut vertical_iter = vertical_evolving_factors();
+        let horizontal = horizontal_iter.next().unwrap();
+        let vertical = vertical_iter.next().unwrap();
+        ImgGen {
+            per_step: per_step,
+            step: 0,
+            i: 0,
+            horizontal_iter: horizontal_iter,
+            vertical_iter: vertical_iter,
+            current_horizontal: horizontal,
+            current_vertical: vertical,
+            rng: rng,
+            static_factors: static_factors().collect::<Vec<DMat<T>>>(),
+        }
+
+    }
+}
+
+impl<'a, R, T, IH, IV> Iterator for ImgGen<'a, R, DMat<T>, IH, IV>
+    where R: 'a + Rng,
+          IH: Iterator<Item=DMat<T>>,
+          IV: Iterator<Item=DMat<T>>,
+          T: Clone + Copy + Zero + One + PartialOrd,
+          Closed01<T>: Rand,
+          DMat<T>: Div<T, Output=DMat<T>>
+{
+    type Item = (usize, usize, DMat<T>);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.i == self.per_step {
+            let horizontal = self.horizontal_iter.next();
+            let vertical = self.vertical_iter.next();
+            self.step += 1;
+            if horizontal.is_none() || vertical.is_none() {
+                return None;
+            }
+
+            self.current_horizontal = horizontal.unwrap();
+            self.current_vertical = vertical.unwrap();
+            self.i = 0;
+        }
+
+        let i = self.i;
+        self.i += 1;
+
+        let mut result = DMat::<T>::new_zeros(10, 10);
+
+        // random linear combination
+        for factor in self.static_factors.iter() {
+            result = result.clone() + (factor.clone() * random01(&mut self.rng));
+        }
+        result = result.clone() + (self.current_horizontal.clone() * random01(&mut self.rng))
+            + (self.current_vertical.clone() * random01(&mut self.rng));
+
+        Some((self.step, i, result.normalize()))
+    }
 }
