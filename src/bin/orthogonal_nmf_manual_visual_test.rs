f4bfec29559e7c153292f50f9b306527d26fd040
Maximilian Krüger <kruemaxi@googlemail.com>
2016-01-13 15:42:36 +0100

    all ready for manual visual test of orthogonal nmf

---
+++ src/bin/orthogonal_nmf_manual_visual_test.rs
@@ -0,0 +1,50 @@
+extern crate onmf;
+use onmf::helpers::{ToImage};
+use onmf::testimage_generator;
+use onmf::testimage_generator::{magnify};
+
+extern crate nalgebra;
+use self::nalgebra::{DMat};
+
+extern crate rand;
+use rand::{StdRng, SeedableRng};
+
+fn main() {
+    let mag_factor = 10;
+
+    let seed: &[_] = &[1, 2, 3, 4];
+    let mut rng: StdRng = SeedableRng::from_seed(seed);
+
+    let steps = 6;
+    let per_step = 10;
+    let ntimes = steps * per_step;
+    let nobserved = 10 * 10;
+    let nhidden = 10;
+    let niterations = 100;
+
+    let mut data = DMat::<f64>::new_zeros(ntimes, nobserved);
+
+    // write on testimage into each row of data
+    for (step, i, factor) in testimage_generator::testimages::<f64, _>(per_step, &mut rng) {
+        let row = step * i;
+        for (col, val) in factor.as_vec().iter().enumerate() {
+            data[(row, col)] = val.clone();
+        }
+    }
+
+    let nmf = onmf::OrthogonalNMF::<f64>::from_data(
+        &data, nhidden, niterations);
+
+    // read testimage out of each row of nmf.hidden
+    for irow in 0..nhidden {
+        let mut column = Vec::<f64>::new();
+        for icol in 0..nobserved {
+            column.push(nmf.hidden[(irow, icol)]);
+        }
+        let image = DMat::<f64>::from_col_vec(10, 10, &column[..]);
+        magnify(image, mag_factor).save_to_png(&format!("orthogonal-nmf-hidden-{}.png", irow)[..]).unwrap();
+
+    }
+    // for (i, factor) in decomposed_hidden.enumerate() {
+    // }
+}
--- src/orthogonal_nmf.rs
+++ src/orthogonal_nmf.rs
@@ -1,14 +1,90 @@
 extern crate nalgebra;
 use self::nalgebra::{DMat};
 
+extern crate rand;
+use self::rand::{Rand};
+
 pub struct OrthogonalNMF<FloatT> {
-    /// maps hidden variables (= rows) to observed variables (= cols).
-    /// changes on every `update`.
-    /// stays constant in size.
+    // TODO add docstrings
     pub hidden: DMat<FloatT>,
-    /// maps times (= rows) to hidden variables (= cols).
-    /// changes on every `update`.
-    /// inspect this.
-    /// will grow in size.
     pub weights: DMat<FloatT>,
+}
+
+impl<FloatT: Rand> OrthogonalNMF<FloatT> {
+    /// one observed per column.
+    /// one time per row.
+    pub fn from_data(data: &DMat<FloatT>, nhidden: usize, niterations: usize) -> OrthogonalNMF<FloatT> {
+        let nobserved = data.ncols();
+        let ntimes = data.nrows();
+        let mut nmf = OrthogonalNMF {
+            hidden: DMat::new_random(nhidden, nobserved),
+            weights: DMat::new_random(ntimes, nhidden),
+        };
+        // TODO how many iterations ?
+        // it gets better and better with each iteration
+        // for i in 0..niterations {
+        //     // TODO alpha goes to infinity
+        //     // detail factor
+        //     // tunable
+        //     let alpha = 0.1 * 1.01.powi(i);
+        //     nmf.iterate(data, alpha);
+        // }
+        nmf
+    }
+
+    // TODO maybe a function to reuse an existing OrthogonalNMF
+    // in order to avoid unnecessary memory allocations
+
+    // TODO initialize with values from last time step t-1 instead of choosing randomly
+    // init_manually
+
+    // TODO compare this to the seoung solution
+    // #[inline]
+    // fn iterate(&mut self, data: &DMat<f64>, alpha: f64) {
+    //     let latents_transposed = self.latents.transpose();
+    //     let weights_transposed = self.weights.transpose();
+    //
+    //     // TODO names
+    //     let weights_foo = data.mul(latents_transposed);
+    //     let weights_bar = self.weights.mul(self.latents).mul(latents_transposed);
+    //
+    //     let latents_foo = weights_transposed.mul(data);
+    //     let size = ?
+    //     let gamma = DMat::from_elem(size, size, alpha);
+    //     let zeroes = DVec::new_zeros(size);
+    //     gamma.set_diag(zeroes);
+    //     let latents_bar = weights_transposed
+    //         .mul(self.weights)
+    //         .mul(self.latents)
+    //         // we add the previous latents
+    //         // multiplied by alpha except for the diag which is set to zero
+    //         .add(gamma.mul(self.latents));
+    //
+    //     // iterate weights
+    //     // TODO correct order ?
+    //     for col in 0..self.weights.ncols() {
+    //         for row in 0..self.weights.nrows() {
+    //             let index = (row, col);
+    //             let foo_value = unsafe { weights_foo.unsafe_at(index) }
+    //             // these get larger and larger
+    //             let bar_value = unsafe { weights_bar.unsafe_at(index) }
+    //             let old_value = unsafe { self.weights.unsafe_at(index) }
+    //             let new_value = old_value * foo_value / bar_value;
+    //             unsafe { self.weights.unsafe_set(index, new_value) }
+    //         }
+    //     }
+    //
+    //     // iterate latents
+    //     // TODO correct order ?
+    //     for col in 0..self.latents.ncols() {
+    //         for row in 0..self.latents.nrows() {
+    //             let index = (row, col);
+    //             let foo_value = latents_foo[index];
+    //             let bar_value = latents_bar[index]
+    //             let old_value = self.latents[index];
+    //             let new_value = old_value * foo_value / bar_value;
+    //             self.latents[index] = new_value;
+    //         }
+    //     }
+    // }
 }
