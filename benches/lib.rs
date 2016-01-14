30a0f8cc81625dc06e86484a5a16e31ad4c59546
Maximilian Krüger <kruemaxi@googlemail.com>
2016-01-14 23:36:02 +0100

    benchmark ortho nmf weights divisor. again blas is 8x faster

--- benches/lib.rs
+++ benches/lib.rs
@@ -7,7 +7,7 @@ extern crate nalgebra;
 use self::nalgebra::{DMat, Transpose};
 
 extern crate rblas;
-use rblas::matrix::ops::Gemm;
+use rblas::matrix::ops::{Gemm, Syrk};
 use rblas::math::mat::Mat;
 use rblas::attribute::Transpose as BlasTranspose;
 
@@ -33,6 +33,38 @@ fn bench_ortho_nmf_weights_dividend_rblas(bencher: &mut test::Bencher) {
         Gemm::gemm(
             &1.,
             BlasTranspose::NoTrans, &samples,
+            BlasTranspose::Trans, &hidden,
+            &0.,
+            &mut result);
+        result
+    });
+}
+
+#[bench]
+fn bench_ortho_nmf_weights_divisor_nalgebra(bencher: &mut test::Bencher) {
+    let weights = DMat::<f64>::new_ones(NSAMPLES, NHIDDEN);
+    let hidden = DMat::<f64>::new_ones(NHIDDEN, NOBSERVED);
+    bencher.iter(|| {
+        weights.clone().mul(&hidden).mul(hidden.transpose())
+    });
+}
+
+#[bench]
+fn bench_ortho_nmf_weights_divisor_rblas(bencher: &mut test::Bencher) {
+    let weights = Mat::<f64>::fill(1., NSAMPLES, NHIDDEN);
+    let hidden = Mat::<f64>::fill(1., NHIDDEN, NOBSERVED);
+    let mut tmp = Mat::<f64>::new(NSAMPLES, NOBSERVED);
+    bencher.iter(|| {
+        let mut result = Mat::<f64>::fill(1., NSAMPLES, NHIDDEN);
+        Gemm::gemm(
+            &1.,
+            BlasTranspose::NoTrans, &weights,
+            BlasTranspose::NoTrans, &hidden,
+            &0.,
+            &mut tmp);
+        Gemm::gemm(
+            &1.,
+            BlasTranspose::NoTrans, &tmp,
             BlasTranspose::Trans, &hidden,
             &0.,
             &mut result);
