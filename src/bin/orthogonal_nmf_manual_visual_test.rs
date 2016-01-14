038c952f5672f244c0de8bf6adf024209e77711e
Maximilian Krüger <kruemaxi@googlemail.com>
2016-01-14 17:32:12 +0100

    ortho nmf: search for right number of hidden vars to not learn mixtures

--- src/bin/orthogonal_nmf_manual_visual_test.rs
+++ src/bin/orthogonal_nmf_manual_visual_test.rs
@@ -19,7 +19,7 @@ fn main() {
     let per_step = 10;
     let nsamples = steps * per_step;
     let nobserved = 10 * 10;
-    let nhidden = 10;
+    let nhidden = 20;
 
     let mut data = DMat::<f64>::new_zeros(nsamples, nobserved);
 
