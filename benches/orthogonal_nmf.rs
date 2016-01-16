a8776637e61ea0ef2c7c186614819726d2110b4f
Maximilian Krüger <kruemaxi@googlemail.com>
2016-01-16 12:01:44 +0100

    add more benchmarks for entire ortho nmf

--- benches/orthogonal_nmf.rs
+++ benches/orthogonal_nmf.rs
@@ -33,17 +33,33 @@ macro_rules! bench_ortho_nmf {
 fn bench_ortho_nmf_f64_10_256_11(bencher: &mut test::Bencher) {
     bench_ortho_nmf!(bencher, f64, 10, 256, 11);
 }
-
 #[bench]
 fn bench_ortho_nmf_f32_10_256_11(bencher: &mut test::Bencher) {
     bench_ortho_nmf!(bencher, f32, 10, 256, 11);
 }
 
 #[bench]
+fn bench_ortho_nmf_f64_10_256_20(bencher: &mut test::Bencher) {
+    bench_ortho_nmf!(bencher, f64, 10, 256, 20);
+}
+#[bench]
+fn bench_ortho_nmf_f32_10_256_20(bencher: &mut test::Bencher) {
+    bench_ortho_nmf!(bencher, f32, 10, 256, 20);
+}
+
+#[bench]
+fn bench_ortho_nmf_f64_10_256_30(bencher: &mut test::Bencher) {
+    bench_ortho_nmf!(bencher, f64, 10, 256, 30);
+}
+#[bench]
+fn bench_ortho_nmf_f32_10_256_30(bencher: &mut test::Bencher) {
+    bench_ortho_nmf!(bencher, f32, 10, 256, 30);
+}
+
+#[bench]
 fn bench_ortho_nmf_f64_10_512_11(bencher: &mut test::Bencher) {
     bench_ortho_nmf!(bencher, f64, 10, 512, 11);
 }
-
 #[bench]
 fn bench_ortho_nmf_f32_10_512_11(bencher: &mut test::Bencher) {
     bench_ortho_nmf!(bencher, f32, 10, 512, 11);
@@ -53,7 +69,6 @@ fn bench_ortho_nmf_f32_10_512_11(bencher: &mut test::Bencher) {
 fn bench_ortho_nmf_f64_20_512_30(bencher: &mut test::Bencher) {
     bench_ortho_nmf!(bencher, f64, 20, 512, 30);
 }
-
 #[bench]
 fn bench_ortho_nmf_f32_20_512_30(bencher: &mut test::Bencher) {
     bench_ortho_nmf!(bencher, f32, 20, 512, 30);
